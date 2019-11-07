931208be711586ee94e65a167266cc10c0c3c68e
demoGit <ngoduchungnaruto@gmail.com>
2019-11-07 20:13:52 +0700

    commit thu 2

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://localhost:27017/doan2';
+// const url = 'mongodb://localhost:27017/doan2';
+const url = "mongodb+srv://admin123:admin123@cluster0-yx6nh.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
+mongoose.connection.once("open", () =>
+  console.log(`Connected to mongo at ${url}`)
+);
